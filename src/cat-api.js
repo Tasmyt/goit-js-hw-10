d27a920057310dd7bb8a03b0869ca2ea27af7912
Tasmyt <121162710+Tasmyt@users.noreply.github.com>
2023-06-29 17:37:58 +0300

    29 06

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,13 +1,13 @@
 
 
-import axios from "axios";
-axios.defaults.headers.common["x-api-key"] = "live_XKa66qKxxyiMOdXAIWhnua9NnnDJ9cM4Ra5gNLnpo1gJEnN0YXAxAqy0CRvdBh9j";
+// import axios from "axios";
+// axios.defaults.headers.common["x-api-key"] = "live_XKa66qKxxyiMOdXAIWhnua9NnnDJ9cM4Ra5gNLnpo1gJEnN0YXAxAqy0CRvdBh9j";
 
 const BASE_URL = `https://api.thecatapi.com/v1`;
-
+const api_key = 'live_XKa66qKxxyiMOdXAIWhnua9NnnDJ9cM4Ra5gNLnpo1gJEnN0YXAxAqy0CRvdBh9j';
 
 function fetchBreeds() {
-    return fetch(`${BASE_URL}/breeds`) 
+    return fetch(`${BASE_URL}/breeds?api_key=${api_key}`) 
          .then(resp => {
             if (!resp.ok) { throw new Error(resp.statusText) }
             return resp.json()
@@ -15,7 +15,7 @@ function fetchBreeds() {
 }
 function fetchCatByBreed(id) {
     
-    return fetch(`${BASE_URL}/images/search?breed_ids=${id}`) 
+    return fetch(`${BASE_URL}/images/search?breed_ids=${id}&api_key=${api_key}`) 
          .then(resp => {
             if (!resp.ok) { throw new Error(resp.statusText) }
             return resp.json()
--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
+import './css/styles.css';
 import API from './cat-api';
 const markup = document.querySelector('.breed-select');
 const catInfoMarkup = document.querySelector('.cat-info');
+const loader = document.querySelector('.loader');
+const errInfo = document.querySelector('.error');
+markup.classList.add('hidden');
+errInfo.classList.add('hidden');
+
+console.log('hello');
+
 markup.addEventListener('change', onSelect);
 API.fetchBreeds()
     .then((data) => (markup.innerHTML = creatSelect(data)))
-    .catch(err => console.log(err));
+    .then(() => {
+        markup.classList.remove('hidden');
+        loader.classList.add('hidden')
+    })
+    .catch(() => ifCatch());
 
 function creatSelect(arr) {
     return arr.map(({id, name}) => `<option value= ${id}>${name}</option>`)
@@ -13,20 +25,25 @@ function creatSelect(arr) {
 
 function onSelect(e) {
     const id = e.currentTarget.value;
-    
+    loader.classList.remove('hidden');
     API.fetchCatByBreed(id)
         .then((data) => (catInfoMarkup.innerHTML = creatCatData(data)))
-        .catch(err => console.log(err));
+        .then(() => loader.classList.add('hidden'))
+        .catch(() => ifCatch());
     
 }
 function creatCatData(arr) {
-    console.log(arr);
-    return arr.map(({ url, breeds: { description, temperament, name }  }) =>
-     `<img src="${url}" alt="${name}">
+    
+    return arr.map(({ url, breeds: {[0]: { description, temperament, name }}}) =>
+        `<img src="${url}" class="image" "alt="${name}">
       <h2>${name}</h2>
       <p>${description}</p>
-      <h3>temperament: </h3>
+      <h3>Temperament: </h3>
       <p>${temperament}</p>`)
         .join('');
-    
+}
+
+function ifCatch() {
+    errInfo.classList.remove('hidden');
+    loader.classList.add('hidden')
 }
